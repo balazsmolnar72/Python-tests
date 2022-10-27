472eb94ed32d2d0a3bc31ad989536420264bb1d8
balazsmolnar72 <balazs.b.molnar@gmail.com>
2022-10-27 18:48:51 +0200

    First version of Sizing algorithm with a printer

--- sizing2.py
+++ sizing2.py
@@ -5,16 +5,15 @@ import math
 import pandas as pd
 import warnings
 
-
-
+# This function sizes the environment
+# Returns a list of dictionaries which shows the number of the configurations needed to cover the requirements
 
 def sizing( no_cores,
             storage_needed,
-            smallestNoRacks=True,
             verbose=False,
             ignore_BaseSystem=True,
             use_DBServerExpansions=True,
-            use_StorageExpansions=True): #This function sizes the environment
+            use_StorageExpansions=True): 
 
     Requirements={
             "MaximumNumberOfOCPUs":no_cores,
@@ -23,31 +22,41 @@ def sizing( no_cores,
 
     Exadata_Configs=pd.read_csv("Exadata X9 Configurations.csv", sep=',').fillna(0)
     exadata_configs_origin=Exadata_Configs.head(4)
-    exadata_configs=exadata_configs_origin.sort_values(by=["NumberOfDatabaseServers"],ascending=False)
+    exadata_configs=exadata_configs_origin.sort_values(by=["NumberOfDatabaseServers"],ascending=False) #Sort Largest configuration first
     del exadata_configs_origin
+    # Expansion configs are the last two lines
     expansion_configs=Exadata_Configs.tail(2)
 
+    # Set some constants
     StorageOCPURatio=float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["TotalUsableDiskCapacity(TB)"]/exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["MaximumNumberOfOCPUs"])
     DBServerCPUSize=int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["MaximumNumberOfOCPUs"]/exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfDatabaseServers"]))
     StorageServerSize=int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["TotalUsableDiskCapacity(TB)"]/exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfStorageServers"]))
     MaxNoStorageServersInRack=int(float(exadata_configs[exadata_configs["Configuration"]=="Full Rack"]["NumberOfStorageServers"]))
     MaxNoDBServersInRack=int(float(exadata_configs[exadata_configs["Configuration"]=="Full Rack"]["NumberOfDatabaseServers"]))
 
+    #Adjust the smallest configuration based on preference (not every environment supports Base Racks)
     if ignore_BaseSystem:
         exadata_configs=exadata_configs[exadata_configs["Configuration"]!="Base system"]
         SmallestConfig="Quarter Rack"
     else:
         SmallestConfig="Base system"
+
+    #Makes a copy of the original requirements to enable later reduction of requirements
     requirements=Requirements.copy()
+
     if verbose:
         print(requirements)
     
+    #Calculating how many Servers are needed and which kind this sizing only takes CPU and Storage requirements into consideration
+    # This is the place to enhance this further to support other parameters as well
     NoDBServers=math.ceil(requirements["MaximumNumberOfOCPUs"]/DBServerCPUSize)
     NoStorageServers=math.ceil(requirements["TotalUsableDiskCapacity(TB)"]/StorageServerSize)
 
+    #initiate the final configuration with an emty list 
     final_config=[]
 
-    if use_DBServerExpansions and use_StorageExpansions:
+    # Different sizing methods based on differnent paramaters 
+    if use_DBServerExpansions and use_StorageExpansions: # If both Storage and Database extensions are allowed
         if requirements["TotalUsableDiskCapacity(TB)"]/requirements["MaximumNumberOfOCPUs"]>StorageOCPURatio: #The configuration is storage heavy
             NoRacks=math.ceil(NoStorageServers/MaxNoStorageServersInRack)
             for item in range(NoRacks):
@@ -62,7 +71,7 @@ def sizing( no_cores,
                         NoStorageServers-=int(float(config["NumberOfStorageServers"]))
                         NoDBServers-=int(float(config["NumberOfDatabaseServers"]))
                         break
-        else:
+        else: #The configuration is CPU heavy
             NoRacks=math.ceil(NoDBServers/MaxNoDBServersInRack)
             for item in range(NoRacks):
                 if NoStorageServers<2:
@@ -77,9 +86,9 @@ def sizing( no_cores,
                         NoDBServers-=int(float(config["NumberOfDatabaseServers"]))
                         break
     elif not use_DBServerExpansions and use_StorageExpansions: # you can use the Storage, but not the DB Server
-        NoRacks=math.ceil(NoStorageServers/MaxNoStorageServersInRack)
-        while NoDBServers>0 or NoRacks>0:
-            if NoDBServers>int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfDatabaseServers"]))*NoRacks:
+        NoRacks=math.ceil(NoStorageServers/MaxNoStorageServersInRack) # Need to calculate how many QRs are needed at least to host all the storage servers
+        while NoDBServers>0 or NoRacks>0: 
+            if NoDBServers>int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfDatabaseServers"]))*NoRacks: #Some of those QRs can be converted to larger configs if we need DB Servers, but only if the required DB servers are more than the Sum of QRs which are needed for Storage expansion anyway
                 for index,config in exadata_configs.iterrows():
                     if int(float(config["NumberOfDatabaseServers"])) <= NoDBServers:
                         final_config.append({"Configuration":config["Configuration"],"Number":1})
@@ -87,69 +96,73 @@ def sizing( no_cores,
                         NoDBServers-=int(float(config["NumberOfDatabaseServers"]))
                         NoRacks-=1
                         break
-            else:
+            else: #If we do not need more DB Servers, but we do not have enough expansion capacities we add QRs
                     final_config.append({"Configuration":"Quarter Rack","Number":1})
                     NoStorageServers-=int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfStorageServers"]))
                     NoDBServers-=int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfDatabaseServers"]))
                     NoRacks-=1
-            if NoDBServers<2:
+            if NoDBServers<2: # if we need less than the smallest configuration we add the smallest configuration, since we cannot use DB Server Expansion
                 final_config.append({"Configuration":SmallestConfig,"Number":1})
                 NoStorageServers-=int(float(exadata_configs[exadata_configs["Configuration"]==SmallestConfig]["NumberOfStorageServers"]))
                 NoDBServers-=int(float(exadata_configs[exadata_configs["Configuration"]==SmallestConfig]["NumberOfDatabaseServers"]))
                 NoRacks-=1
 
-    elif not use_StorageExpansions and use_DBServerExpansions: #You can use DB Server but not the storage
-        NoRacks=math.ceil(NoDBServers/MaxNoDBServersInRack)
+    elif not use_StorageExpansions and use_DBServerExpansions: #You can use DB Server expansion but not the storage server expansion
+        NoRacks=math.ceil(NoDBServers/MaxNoDBServersInRack) # Calculate how many QRs are needed to host the DB Servers
         while NoStorageServers>0 or NoRacks>0:
-            if NoStorageServers>int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfStorageServers"]))*NoRacks:
-                for index,config in exadata_configs.iterrows():
+            if NoStorageServers>int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfStorageServers"]))*NoRacks: # If we need more storage than what we would have because of the DB Servers
+                for index,config in exadata_configs.iterrows(): # We can upgrade the QRs to larger configurations if we need them because of storage
                     if int(float(config["NumberOfStorageServers"])) <= NoStorageServers:
                         final_config.append({"Configuration":config["Configuration"],"Number":1})
                         NoStorageServers-=int(float(config["NumberOfStorageServers"]))
                         NoDBServers-=int(float(config["NumberOfDatabaseServers"]))
                         NoRacks-=1
                         break
-            else:
+            else: # If we do not need more storage then we'll add just QRs to be able to host the DB Server Expansions
                     final_config.append({"Configuration":"Quarter Rack","Number":1})
                     NoStorageServers-=int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfStorageServers"]))
                     NoDBServers-=int(float(exadata_configs[exadata_configs["Configuration"]=="Quarter Rack"]["NumberOfDatabaseServers"]))
                     NoRacks-=1
-            if NoStorageServers<3:
+            if NoStorageServers<3: # If we need less storage than the minimum configuration we add the minimum configuration
                 final_config.append({"Configuration":SmallestConfig,"Number":1})
                 NoStorageServers-=int(float(exadata_configs[exadata_configs["Configuration"]==SmallestConfig]["NumberOfStorageServers"]))
                 NoDBServers-=int(float(exadata_configs[exadata_configs["Configuration"]==SmallestConfig]["NumberOfDatabaseServers"]))
                 NoRacks-=1
 
                     
-    else:
+    else: # If we cannot use neighter Storage nor DB Server expansion
         while NoStorageServers>0 or NoDBServers>0:
-            if (NoStorageServers<3 and NoDBServers<=0) or (NoDBServers<2 and NoStorageServers<=0):
+            if (NoStorageServers<3 and NoDBServers<=0) or (NoDBServers<2 and NoStorageServers<=0): # If we need less than the minimum config we add the minimum config
                 final_config.append({"Configuration":SmallestConfig,"Number":1})
                 NoStorageServers-=int(float(exadata_configs[exadata_configs["Configuration"]==SmallestConfig]["NumberOfStorageServers"]))
                 NoDBServers-=int(float(exadata_configs[exadata_configs["Configuration"]==SmallestConfig]["NumberOfDatabaseServers"]))
                 continue
-            for index,config in exadata_configs.iterrows():
+            for index,config in exadata_configs.iterrows(): # find the best fit for the requirement
                 if int(float(config["NumberOfDatabaseServers"])) <= NoStorageServers or int(float(config["NumberOfDatabaseServers"])) <= NoDBServers:
                     final_config.append({"Configuration":config["Configuration"],"Number":1})
                     NoStorageServers-=int(float(config["NumberOfStorageServers"]))
                     NoDBServers-=int(float(config["NumberOfDatabaseServers"]))
                     break
-    if NoDBServers>0:
+    if NoDBServers>0: # add the remaining DB Server Expansions 
         final_config.append({"Configuration":"DB Server Expansion","Number":NoDBServers})
-    if NoStorageServers>0:
+    if NoStorageServers>0: # add the remaining Storage Server Expansions 
         final_config.append({"Configuration":"Storage Expansion","Number":NoStorageServers})
     
+    # Let's consolidate the result and group the same configurations together
+
     final_config=sorted(final_config, key=lambda val: val["Configuration"])
     for index in range(len(final_config)-1):
         while index<len(final_config)-1 and final_config[index]["Configuration"]==final_config[index+1]["Configuration"]:
             final_config[index]["Number"]+=final_config[index+1]["Number"]
             del final_config[index+1]
     
+    # Now we have the sizing. Let's sort it and add some useful parameters to the list
     def sortByConfigSize(e):
             if e["Configuration"] in exadata_configs["Configuration"].to_list():
                 return 3-exadata_configs[exadata_configs["Configuration"]==e["Configuration"]].index.to_list()[0]
             else:
                 return expansion_configs[expansion_configs["Configuration"]==e["Configuration"]].index.to_list()[0]
+
     final_config.sort(key=sortByConfigSize)
     for config in final_config:
         if("Expansion" not in config["Configuration"]):
@@ -161,12 +174,11 @@ def sizing( no_cores,
             config["CPUSize"]=int(float(expansion_configs[expansion_configs["Configuration"]==config["Configuration"]]["MaximumNumberOfOCPUs"])*int(config["Number"]))
             config["StorageSize"]=int(float(expansion_configs[expansion_configs["Configuration"]==config["Configuration"]]["TotalUsableDiskCapacity(TB)"])*int(config["Number"]))
         config["Cost"]=int(exadata_prices[exadata_prices["Configuration"]== config["Configuration"]]["Price"])*int(config["Number"])
-    return final_config
+    return final_config #return the result 
 
  
 no_cores=650
 storage_needed=650*1.5
-
 
 Exadata_Configs=pd.read_csv("Exadata X9 Configurations.csv", sep=',').fillna(0)
 exadata_configs=Exadata_Configs.head(4)
@@ -177,29 +189,20 @@ MaxNoDBServersInRack=int(float(exadata_configs[exadata_configs["Configuration"]=
 
 final_config=sizing(    no_cores,
                         storage_needed,
-                        smallestNoRacks=True,
                         ignore_BaseSystem=True,
                         verbose=False,
                         use_DBServerExpansions=False,
                         use_StorageExpansions=True)
 
-# print the final configuration grouped
+
 print("{:<20}{:<5}{:<5}{:<5}\t{}".format("Configuration","Number","CPU","Storage","Monthly Cost"))
 print("-"*50)
-totalCPU=0
-totalStorage=0
-totalNumber=0
-totalCost=0
-freeStorageExpansion=freeDBServerExpansion=0
+totalCPU=totalStorage=totalNumber=totalCost=freeStorageExpansion=freeDBServerExpansion=0 #initalize counters
 for config in final_config:
     if("Expansion" not in config["Configuration"]):
-        CPUSize=int(float(exadata_configs[exadata_configs["Configuration"]==config["Configuration"]]["MaximumNumberOfOCPUs"])*int(config["Number"]))
-        StorageSize=int(float(exadata_configs[exadata_configs["Configuration"]==config["Configuration"]]["TotalUsableDiskCapacity(TB)"])*int(config["Number"]))
         freeStorageExpansion+=int(float(exadata_configs[exadata_configs["Configuration"]==config["Configuration"]]["AdditionalStorageCellPossibility"])*int(config["Number"]))
         freeDBServerExpansion+=int(float(MaxNoDBServersInRack-exadata_configs[exadata_configs["Configuration"]==config["Configuration"]]["NumberOfDatabaseServers"])*int(config["Number"]))
     else:
-        CPUSize=int(float(expansion_configs[expansion_configs["Configuration"]==config["Configuration"]]["MaximumNumberOfOCPUs"])*int(config["Number"]))
-        StorageSize=int(float(expansion_configs[expansion_configs["Configuration"]==config["Configuration"]]["TotalUsableDiskCapacity(TB)"])*int(config["Number"]))
         if "Storage" in config["Configuration"]:
             freeStorageExpansion-=config["Number"]
         else:
