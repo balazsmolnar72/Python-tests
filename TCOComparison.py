63ab357184b5dd79ba68073ec1447afbce6da4d8
balazsmolnar72 <balazs.b.molnar@gmail.com>
2022-11-11 15:27:55 +0100

    Tailor to OCI 1

--- TCOComparison.py
+++ TCOComparison.py
@@ -1,5 +1,7 @@
 import json
+
 import pandas as pd
+
 import ExaSizing as sizer
 
 
@@ -10,7 +12,7 @@ def TCO_comparison(cores,storageTB,annualDBsupport,utilization, Exa_Core_Ratio=1
     exadata_prices=pd.read_csv("ExaCC pricing.csv", sep=',').fillna(0)
     exaCSResult=sizer.sizing(no_cores=cores,storage_needed=storageTB,Exa_Core_Ratio=Exa_Core_Ratio)
     ExaCSInfraCost=sum(config["Cost"] for config in exaCSResult)*12
-    ExaCCResult=sizer.sizing(no_cores=cores,storage_needed=storageTB,use_DBServerExpansions=False)
+    ExaCCResult=sizer.sizing(no_cores=cores,storage_needed=storageTB,use_DBServerExpansions=False,Exa_Core_Ratio=Exa_Core_Ratio)
     ExaCCInfraCost=sum(config["Cost"] for config in ExaCCResult)*12
     OnPremInfraCost=cores*benchmarks["Cost of 1 core Intel Server Acquisition"]/benchmarks["Amortization Period in Years"]+storageTB*benchmarks["Cost of TB Storage CAPEX"]/benchmarks["Amortization Period in Years"]+cores*benchmarks["Support of 1 TB of Storage OPEX"]+storageTB*benchmarks["OS and Server support for 1 core"]
     UniversalCreditLI=0
