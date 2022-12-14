4d6c8fceeaad9ed54323fcec667456d06af3f216
balazsmolnar72 <balazs.b.molnar@gmail.com>
2022-12-14 19:04:35 +0100

    Webserver version of the License Analysis can print HTML as well.

--- WebServer/cgi-bin/upload_script.py
+++ WebServer/cgi-bin/upload_script.py
@@ -1,8 +1,8 @@
-import csv
+import csv,string 
 
 def is_csv(infile):
     try:
-        with open(infile, newline='') as csvfile:
+        with open(infile, newline='', encoding="utf-8") as csvfile:
             start = csvfile.read(4096)
 
             # isprintable does not allow newlines, printable does not allow umlauts...
@@ -14,9 +14,16 @@ def is_csv(infile):
         # Could not get a csv dialect -> probably not a csv.
         return False
 
-import os, cgi
-import LicenseAnalysis as LA
+import os, cgi, time
+
 form = cgi.FieldStorage() 
+
+print("Content-type:text/html\r\n\r\n")
+print('<html>')
+print('<head>')
+print('<title>TCO Analysis</title>')
+print('</head>')
+print('<body>')
 
 fi = form['filename']
 fn = fi.filename
@@ -27,27 +34,25 @@ if fi.filename:
     fo=open(fn, 'wb')
     fo.write(fi.file.read())
     fo.close()
+    while not os.path.exists(fn): # Wait until the file appear in File system (probably not needed)
+        print("file does not exists")
+        time.sleep(1)
     
 
+import LicenseAnalysis as LA
 
-print("Content-type:text/html\r\n\r\n")
-print('<html>')
-print('<head>')
-print('<title>TCO Analysis</title>')
-print('</head>')
-print('<body>')
 
-if is_csv(fn):
+""" if is_csv(fn):
     print("You've uploaded a csv file")
 else:
-    print("The file you've uploded is not a csv.")
+    print("The file you've uploded is not a csv.") """
 
 print('<h2>This is a TCO analysis</h2>')
 
 LA.printNoCustomers()
 
 print('<h2>These customers or this customer pays annually this amount of Support to Oracle for Oracle Database:</h2>')
-LA.printLicenseCosts()
+LA.printLicenseCosts(style='html')
 
 print('</body>')
 print('</html>')
